00c49a0e2e36aa44419575a18486c718a79c704c
enzoobona <ebonafine@frba.utn.edu.ar>
2026-04-09 13:19:53 +0000

    Etapa 3 Prueba 1

--- client/src/client.c
+++ client/src/client.c
@@ -30,21 +30,19 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
+	config = config_create("./cliente.config");
+
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
+
 	// Loggeamos el valor de config
 
-	config = config_create("./cliente.config");
-	char* key_value = config_get_string_value(config, "CLAVE");
-
-	log_info(logger, key_value);
-
-
+	log_info(logger, valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
 	leer_consola(logger);
-
-	log_destroy(logger);
-	config_destroy(config);
 
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
 
@@ -56,6 +54,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor, conexion);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
@@ -109,17 +108,30 @@ void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
-
+	while(1){
+		leido = readline("> ");
+		if(strcmp(leido, "") == 0){
+			break;
+		}
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+		free(leido);
+	}
+	
+	enviar_paquete(paquete, conexion);
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+
+	log_destroy(logger);
+	config_destroy(config);
+	liberar_conexion(conexion);
 }
